77e3c40ef01527092ac0f24b86fc4d3b8c49303f
jonatanlima06 <jonatan.rtp@gmail.com>
2025-02-10 10:32:12 -0300

    Finalizando a tela de login

--- sispar/src/components/login/Login.jsx
+++ sispar/src/components/login/Login.jsx
@@ -1,31 +1,28 @@
-import Capa from "../../assets/Tela Login/imagem tela de login.png";
 import Logo from "../../assets/Tela Login/logo-ws.png";
+import styles from "./Login.module.scss"
 
-function Login () {
-    return(
+function Login() {
+    return (
         <main>
-        <section>
-            <img src={Capa} alt="Capa" /> 
-            <p>Section vazia para receber o background do navio</p> 
-        </section>
-        <section> 
-            <img src={Logo} alt="Logo da Wilson Sons" />
-            <h1>Boas vindas ao Novo Portal SISPAR</h1>
-            <p>Sistema de Emissão de Boletos e Parcelamento</p>
-            <form action="">
-                <input type="email" name="email" id="email" placeholder="Email"/>
-                <input type="password" name="password" id="password" placeholder="Senha"/>
-                <a href="">Esqueci minha senha</a>
-            </form>
-            <div>
-                <button>Entrar</button>
-                <button>Criar conta</button>
-            </div>
+            <section className={styles.containerLogin}>
+
+            </section>
+            <section className={styles.containerForm}>
+                <img src={Logo} alt="Logo da Wilson Sons" />
+                <h1>Boas vindas ao Novo Portal SISPAR</h1>
+                <p>Sistema de Emissão de Boletos e Parcelamento</p>
+                <form action="">
+                    <input type="email" name="email" id="email" placeholder="Email" />
+                    <input type="password" name="password" id="password" placeholder="Senha" />
+                    <a href="">Esqueci minha senha</a>
+                    <div className={styles.buttonGroup}>
+                        <button>Entrar</button>
+                        <button>Criar conta</button>
+                    </div>
+                </form>
+            </section>
 
 
-        </section>
-        
-        
         </main>
     )
 }
