e8f26783f927dd4eac9749263407baf9dc51f0e2
jonatanlima06 <jonatan.rtp@gmail.com>
2025-02-21 19:31:01 -0300

    Finalizando a tela de reembolso

--- sispar/src/components/solicitacao/Solicitacao.jsx
+++ sispar/src/components/solicitacao/Solicitacao.jsx
@@ -2,6 +2,8 @@ import NavBar from "../navbar/NavBar.jsx"
 import Home from "../../assets/Header/home_header.png"
 import Seta from "../../assets/Dashboard/Vector_seta.png"
 import styles from "./Solicitacao.module.scss"
+import Lixeira from "../../assets/Solicitacao/lixeira.png"
+import Motivo from "../../assets/Solicitacao/motivo.png"
 
 
 function Solicitacao() {
@@ -46,26 +48,123 @@ function Solicitacao() {
                                 <input type="date" name="" id="data" />
                             </div>
                             <div className={styles.selectDespesa}>
+                                <label htmlFor=""> Tipo de despesa </label>
                                 <select name="" id="">
                                     <option value=""> Selecionar </option>
                                     <option value=""> Alimentação </option>
                                     <option value=""> Combustível </option>
-                                    <option value="">  </option>
-                                    <option value=""></option>
-                                    <option value=""></option>
-                                    <option value=""></option>
+                                    <option value=""> Condução </option>
+                                    <option value=""> Estacionamento </option>
+                                    <option value=""> Viagem Admin. </option>
+                                    <option value=""> Viagem Operacional </option>
+                                    <option value=""> Eventos de representação </option>
                                 </select>
                             </div>
-
+                            <div className={styles.centroDeCusto}>
+                                <label htmlFor=""> Centro de Custo </label>
+                                <select name="" id="">
+                                    <option value=""> Selecionar</option>
+                                    <option value="">
+                                        1100109002 - FIN CONTROLES INTERNOS MTZ
+                                    </option>
+                                    <option value="">
+                                        1100110002 - FIN VICE-PRESIDENCIA FINANCAS MTZ
+                                    </option>
+                                    <option value="">1100110101 - FIN CONTABILIDADE MTZ</option>
+                                </select>
+                            </div>
+                            <div className={styles.ordem}>
+                                <label htmlFor=""> Ord. Int. </label>
+                                <input type="number" name="" id="" />
+                            </div>
+                            <div className={styles.divisoes}>
+                                <label htmlFor=""> Div. </label>
+                                <input type="number" name="" id="" />
+                            </div>
+                            <div className={styles.pep}>
+                                <label htmlFor=""> PEP </label>
+                                <input type="number" name="" id="" />
+                            </div>
+                            <div className={styles.moeda}>
+                                <label htmlFor=""> Moeda </label>
+                                <select name="" id="">
+                                    <option value="">Selecionar</option>
+                                    <option value="">BRL</option>
+                                    <option value="">ARS</option>
+                                    <option value="">USD</option>
+                                </select>
+                            </div>
+                            <div className={styles.distancia}>
+                                <label htmlFor=""> Dist / KM </label>
+                                <input type="text" />
+                            </div>
+                            <div className={styles.valorKm}>
+                                <label htmlFor=""> Valor / KM </label>
+                                <input type="number" name="" id="" />
+                            </div>
+                            <div className={styles.valorFaturado}>
+                                <label htmlFor=""> Val. Faturado </label>
+                                <input type="number" name="" id="" />
+                            </div>
+                            <div className={styles.despesa}>
+                                <label htmlFor=""> Despesa </label>
+                                <input type="number" name="" id="" />
+                            </div>
+                            <div className={styles.botoes}>
+                                <button className={styles.botaoSalvar}> + Salvar </button>
+                                <button className={styles.botaoDeletar}> Del </button>
+                            </div>
                         </div>
-
                     </form>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Colaborador(a)</th>
+                                <th>Empresa</th>
+                                <th>Nº Prest.</th>
+                                <th>Data</th>
+                                <th>Motivo</th>
+                                <th>Tipo de despesa</th>
+                                <th>Ctr. Custo</th>
+                                <th>Ord. Int.</th>
+                                <th>Div.</th>
+                                <th>PEP</th>
+                                <th>Moeda</th>
+                                <th>Dist. KM</th>
+                                <th>Val. KM</th>
+                                <th>Val. Faturado</th>
+                                <th>Despesa</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td><img src={Lixeira} alt="Lixeira" /></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                        </tbody>
+
+
+
+                    </table>
+
 
                 </main>
-
             </div>
-
-
         </div>
     )
 }
